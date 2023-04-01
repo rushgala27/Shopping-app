a71d71c5019e50588822e1b0a15c50037596c191
rushgala27 <84933381+rushgala27@users.noreply.github.com>
2023-04-01 13:41:44 +0530

    Initial commit

---
+++ src/misc/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCklo5_ha_vDWkJDjm55Xqw0tYZlI91Mz4",
+  authDomain: "shopping-app-cae08.firebaseapp.com",
+  projectId: "shopping-app-cae08",
+  storageBucket: "shopping-app-cae08.appspot.com",
+  messagingSenderId: "68327226031",
+  appId: "1:68327226031:web:34d92331e47be1f68d325a",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
